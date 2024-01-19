135c516119c6422cf3e8fd6d2ecf1bce69c404d8
Harteyou <raidingsquire54@gmail.com>
2024-01-19 11:33:25 +0700

    push

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+// navbar Fixed
+window.onscroll = function(){
+    const header = document.querySelector('header');
+    const fixed_navbar = header
+}
+// Hamburger
+const hamburger = document.querySelector('#hamburger');
+
+hamburger. addEventListener('click', function(){
+    hamburger.classList.toggle('hamburger-active')
+});
