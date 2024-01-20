a56432708bc6f15f142c52457df62fd7ed8fb771
Harteyou <raidingsquire54@gmail.com>
2024-01-20 18:05:06 +0700

    Rayhan

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,20 @@
 // navbar Fixed
 window.onscroll = function(){
     const header = document.querySelector('header');
-    const fixed_navbar = header
-}
+    const fixed_navbar = header.offsetTop;
+
+    if(window.pageYOffset > fixed_navbar) {
+        header.classList.add('navbar-fixed');
+    }
+    else{
+        header.classList.remove('navbar-fixed');
+    }
+};
 // Hamburger
 const hamburger = document.querySelector('#hamburger');
+const navbar_menu = document.querySelector('#nav-menu');
 
 hamburger. addEventListener('click', function(){
     hamburger.classList.toggle('hamburger-active')
+    navbar_menu.classList.toggle('hidden');
 });
