593a7eda2ad89fc5bc7751895c81d38b5698ace1
Harteyou <raidingsquire54@gmail.com>
2024-01-24 23:10:28 +0700

    Rayhan Raya Farabi

--- src/js/script.js
+++ src/js/script.js
@@ -18,3 +18,18 @@ hamburger. addEventListener('click', function(){
     hamburger.classList.toggle('hamburger-active')
     navbar_menu.classList.toggle('hidden');
 });
+
+function SendMail(event) {
+    emailjs.init("z6bzcraJrh6Gieehq");
+    event.preventDefault();  // Prevent default form submission
+    var parameters = {
+        Nama : document.getElementById('Nama').value,
+        Email : document.getElementById('Email').value,
+        pesan : document.getElementById('pesan').value
+    };
+
+    emailjs.send("service_cpajmkc", "template_qlce284", parameters)
+    .then(function(response) {
+        sessionStorage.setItem("Success", "Terima Kasih Telah Mengirim Email");
+    }
+)};
